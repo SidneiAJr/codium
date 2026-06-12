52dc78a50667a85fa9fd5c0c049a67aac5c17a28
Sid Jr <sidalbertok@gmail.com>
2026-06-12 13:06:47 -0300

    Create Conexcao_banco.java

---
+++ Java/Java Backend/Spring Boot Funcional Sem Pacote/Conexcao_banco.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject1;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+public class Conexcao_banco {
+
+}
