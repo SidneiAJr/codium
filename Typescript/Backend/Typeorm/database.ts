81e33b45d1eb51c0c807944fa41ce78639c77644
Sid Jr <sidalbertok@gmail.com>
2026-06-13 18:02:59 -0300

    Create database.ts

---
+++ Typescript/Backend/Typeorm/database.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { Usuario } from './Livro';
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "bancox",
+    synchronize: true, 
+    logging: true,
+    entities: [Usuario],
+});
